77ec5cde2ff3f20a391b2ea062112e21e3cfb264
Peter Hintze <hintze.peter@gmail.com>
2025-01-31 11:17:13 -0500

    Database override removed

--- server/src/seeds/index.ts
+++ server/src/seeds/index.ts
@@ -4,7 +4,7 @@ import { sequelize } from '../models/index.js';
 
 const seedAll = async (): Promise<void> => {
   try {
-    await sequelize.sync({ force: true });
+    await sequelize.sync({ force: false });
     console.log('\n----- DATABASE SYNCED -----\n');
     
     await seedUsers();
